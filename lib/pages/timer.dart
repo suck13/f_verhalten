f382026f0aaa50f612e1267fc5bd82d3caf9f573
suck13 <56693990+suck13@users.noreply.github.com>
2022-01-07 22:47:58 -0300

    Add files via upload
    code by fer

---
+++ lib/pages/timer.dart
@@ -0,0 +1,62 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+
+class OtpTimer extends StatefulWidget {
+  OtpTimer({Key? key}) : super(key: key);
+
+  @override
+  _OtpTimerState createState() => _OtpTimerState();
+}
+
+class _OtpTimerState extends State<OtpTimer> {
+  final interval = const Duration(seconds: 1);
+
+  final int timerMaxSeconds = 60;
+
+  int currentSeconds = 0;
+
+  String get timerText =>
+      '${((timerMaxSeconds - currentSeconds) ~/ 60).toString().padLeft(2, '0')}: ${((timerMaxSeconds - currentSeconds) % 60).toString().padLeft(2, '0')}';
+
+  startTimeout([int? milliseconds]) {
+    var duration = interval;
+    Timer.periodic(duration, (timer) {
+      setState(() {
+        print(timer.tick);
+        currentSeconds = timer.tick;
+        if (timer.tick >= timerMaxSeconds) timer.cancel();
+      });
+    });
+  }
+
+  @override
+  void initState() {
+    startTimeout();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        title: Text('Cronômetro'),
+      ),
+      body: Container(
+        height: double.infinity,
+        width: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(Icons.timer),
+            SizedBox(
+              width: 15,
+              height: 15,
+            ),
+            Text(timerText),
+          ],
+        ),
+      ),
+    );
+  }
+}
