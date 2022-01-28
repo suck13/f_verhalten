6004f5a7649cd334af072c717e6d747433ab3f4d
suck13 <56693990+suck13@users.noreply.github.com>
2022-01-28 00:36:43 -0300

    Update sobre.dart

--- lib/pages/sobre.dart
+++ lib/pages/sobre.dart
@@ -10,13 +10,24 @@ class SobrePage extends StatelessWidget {
         automaticallyImplyLeading: false,
         backgroundColor: Colors.transparent,
         elevation: 0,
-        title: Text('Sobre o Grupo:'),
+        title: Text(
+          'Sobre o Grupo:',
+          style: TextStyle(
+            color: Colors.redAccent,
+            fontSize: 25.00,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
       ),
       body: Container(
         height: double.infinity,
         width: double.infinity,
         child: Center(
-          child: Text("Funcionalidade em desenvolvimento..."),
+          child: Image.asset(
+            'lib/assets/images/sobre.png',
+            width: 384,
+            height: 363.8,
+          ),
         ),
       ),
     );
